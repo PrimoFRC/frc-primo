c844fb441044ae7f9dc94d09d1aab7ee7a2f0e53
yuval1go@gmail.com <yuval1go@gmail.com@d99c8834-613a-db42-c28d-8af20e7fc61e>
2014-01-28 20:35:02 +0000

    MoveArmDown/Free Free- לא גמור

--- branches/AerialAssistPRIMO4586/src/il/co/primo4586/frc2014/commands/collector/MoveArmFree.java
+++ branches/AerialAssistPRIMO4586/src/il/co/primo4586/frc2014/commands/collector/MoveArmFree.java
@@ -13,7 +13,10 @@ import il.co.primo4586.frc2014.commands.CommandBase;
  */
 public class MoveArmFree extends CommandBase {
 
+	// not finished
 	int direction;
+	private boolean finished;
+
 
     public MoveArmFree(int direction) {
         // Use requires() here to declare subsystem dependencies
@@ -24,19 +27,39 @@ public class MoveArmFree extends CommandBase {
 
     // Called just before this Command runs the first time
     protected void initialize() {
+		finished = false;
+		finished = (collector.getLevel() == 0);
+		finished = (collector.getLevel() == 2);
 
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+
+		if(!(collector.getBottomMicro()) && !(collector.getTopMicro()))
+		{
 		collector.moveArm(direction);
+		}
+		else
+		{
+			finished = true;
+		}
+
+		 if(getMiddleMicro())
+		 {
+			 if (direction > 0)
+				 collector.setLevel(1.5);
+			 else
+				 collector.setLevel(0.5);
+
+		 }
 
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
 
-        return !oi.manualLowerCollectorArm.get() && !oi.manualRaiseCollectorArm.get();
+        return (!oi.manualLowerCollectorArm.get() && !oi.manualRaiseCollectorArm.get()) || finished;
     }
 
     // Called once after isFinished returns true
