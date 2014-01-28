1fc2c6a02b0ed5e6d6569a270ea3223f11dd0c4d
yuval1go <yuval1go@gmail.com>
2014-01-28 19:47:33 +0000

    collector added

--- branches/AerialAssistPRIMO4586/src/il/co/primo4586/frc2014/OI.java
+++ branches/AerialAssistPRIMO4586/src/il/co/primo4586/frc2014/OI.java
@@ -5,6 +5,9 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.Button;
 import edu.wpi.first.wpilibj.buttons.DigitalIOButton;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
+import il.co.primo4586.frc2014.commands.collector.MoveArmDown;
+import il.co.primo4586.frc2014.commands.collector.MoveArmFree;
+import il.co.primo4586.frc2014.commands.collector.MoveArmUp;
 
 /**
  * This class is the glue that binds the controls on the physical operator
@@ -17,39 +20,55 @@ public class OI {
     // number it is.
     // Joystick stick = new Joystick(port);
     // Button button = new JoystickButton(stick, buttonNumber);
-    
+
     // Another type of button you can create is a DigitalIOButton, which is
     // a button or switch hooked up to the cypress module. These are useful if
     // you want to build a customized operator interface.
     // Button button = new DigitalIOButton(1);
-    
+
     // There are a few additional built in buttons you can use. Additionally,
     // by subclassing Button you can create custom triggers and bind those to
     // commands the same as any other Button.
-    
+
     //// TRIGGERING COMMANDS WITH BUTTONS
     // Once you have a button, it's trivial to bind it to a button in one of
     // three ways:
-    
+
     // Start the command when the button is pressed and let it run the command
     // until it is finished as determined by it's isFinished method.
     // button.whenPressed(new ExampleCommand());
-    
+
     // Run the command while the button is being held down and interrupt it once
     // the button is released.
     // button.whileHeld(new ExampleCommand());
-    
+
     // Start the command when the button is released  and let it run the command
     // until it is finished as determined by it's isFinished method.
     // button.whenReleased(new ExampleCommand());
     public Joystick drivingStick;
-    public JoystickButton button1, button2, button3, button4;
-    
+	public Joystick operatorStick;
+    public JoystickButton button1, button2, manualLowerCollectorArm, manualRaiseCollectorArm, lowerCollectorArm, raiseCollectorArm ;
+
     public OI()
     {
         drivingStick = new Joystick(1);
+		button1 = new JoystickButton(operatorStick, 1);
+		button2 = new JoystickButton(operatorStick, 2);
+		manualLowerCollectorArm = new JoystickButton(operatorStick, 3);
+		manualRaiseCollectorArm = new JoystickButton(operatorStick, 4);
+		lowerCollectorArm = new JoystickButton(operatorStick, 5);
+		raiseCollectorArm = new JoystickButton(operatorStick, 6);
+
+
+
+		lowerCollectorArm.whenPressed(new MoveArmDown());
+		raiseCollectorArm.whenPressed(new MoveArmUp());
+
+		manualLowerCollectorArm.whileHeld(new MoveArmFree(1));
+		manualRaiseCollectorArm.whileHeld(new MoveArmFree(-1));
+
     }
-    
+
     public Joystick getDrivingStick()
     {
         return drivingStick;
--- branches/AerialAssistPRIMO4586/src/il/co/primo4586/frc2014/commands/collector/MoveArmFree.java
+++ branches/AerialAssistPRIMO4586/src/il/co/primo4586/frc2014/commands/collector/MoveArmFree.java
@@ -12,28 +12,38 @@ import il.co.primo4586.frc2014.commands.CommandBase;
  * @author Gottlieb
  */
 public class MoveArmFree extends CommandBase {
-    
-    public MoveArmFree() {
+
+	int direction;
+
+    public MoveArmFree(int direction) {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
+		requires(collector);
+		this.direction = direction;
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
+
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+		collector.moveArm(direction);
+
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+
+        return !oi.manualLowerCollectorArm.get() && !oi.manualRaiseCollectorArm.get();
     }
 
     // Called once after isFinished returns true
-    protected void end() {
-    }
+    protected void end()
+	{
+		collector.moveArm(0);
+	}
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
