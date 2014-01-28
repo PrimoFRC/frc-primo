0b6c92875f71839eb39f476e9b64a0982c686ada
yuval1go <yuval1go@gmail.com>
2014-01-28 17:03:49 +0000

    collector added

--- src/il/co/primo4586/frc2014/commands/collector/MoveArmDown.java
+++ src/il/co/primo4586/frc2014/commands/collector/MoveArmDown.java
@@ -6,33 +6,45 @@
 package il.co.primo4586.frc2014.commands.collector;
 
 import il.co.primo4586.frc2014.commands.CommandBase;
-
 /**
  *
  * @author Gottlieb
  */
 public class MoveArmDown extends CommandBase {
-    
+
+	private boolean finished;
+
     public MoveArmDown() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
+		requires(collector);
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
+		finished = false;
+		finished = (collector.getLevel() == 0);
+
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+		if (!finished)
+			collector.moveArm(-1);
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+		finished &= collector.getMiddleMicro();
+		finished &= collector.getBottomMicro();
+
+		return finished;
     }
 
     // Called once after isFinished returns true
     protected void end() {
+		collector.moveArm(0);
+		collector.setLevel(collector.getLevel()-1);
     }
 
     // Called when another command which requires one or more of the same
