f548b3a5d2c8fb50f6740a5c5ce6d169bbe2fb9f
tomsegal10 <tomsegal10@gmail.com>
2014-01-30 14:38:37 +0000

    changed names to capital latters

---
+++ branches/AerialAssistPRIMO4586/src/il/co/primo4586/frc2014/commands/Shooter/Release.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package il.co.primo4586.frc2014.commands.Shooter;
+
+
+import il.co.primo4586.frc2014.commands.CommandBase;
+
+/**
+ *
+ * @author mor meitar idan
+ */
+public class Release extends CommandBase {
+    //Talon releaser;
+    //DigitalInput releaserLock;
+    public Release() 
+    {
+        requires (shooter);
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() 
+    {
+        shooter.release(0.2);
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() 
+   {
+       
+   }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() 
+    {
+        return !shooter.getLockMicro();
+       
+    }
+
+    // Called once after isFinished returns true
+    protected void end() 
+    {   
+        shooter.release(0);
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() 
+    {
+    
+    }
+}
