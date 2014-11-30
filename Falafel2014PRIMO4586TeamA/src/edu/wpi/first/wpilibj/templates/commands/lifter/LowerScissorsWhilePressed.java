707f9e218ac48bf2458a10b4d3d9c8b9d79f949a
tomsegal10@gmail.com <tomsegal10@gmail.com@d99c8834-613a-db42-c28d-8af20e7fc61e>
2014-11-30 16:10:56 +0000

    Port fixes and SmartDashboard Additions and LowerScissorsWhilePressed isFinished fixed

--- Falafel2014PRIMO4586TeamA/src/edu/wpi/first/wpilibj/templates/commands/lifter/LowerScissorsWhilePressed.java
+++ Falafel2014PRIMO4586TeamA/src/edu/wpi/first/wpilibj/templates/commands/lifter/LowerScissorsWhilePressed.java
@@ -28,7 +28,7 @@ public class LowerScissorsWhilePressed extends CommandBase {
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return (!oi.raiseScissors.get() ||lifter.getBottom() );
+        return (!oi.lowerScissors.get() ||lifter.getBottom() );
     }
 
     // Called once after isFinished returns true
